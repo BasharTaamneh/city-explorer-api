acbad0b5496f1061937dcd3081fa85aaa1377e5f
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-08-21 12:19:09 +0300

    creat cach memo

--- movieShow.js
+++ movieShow.js
@@ -1,27 +1,38 @@
 'use strict';
+let inMemory = {}
 
 const axios=require('axios')
+
 
 function getmoviesData(req, response) {
     let movieSearchcity = req.query.query;
     //https://api.themoviedb.org/3/search/movie?api_key={.....}&query={city_nam}
     let moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movieSearchcity.toLowerCase()}`
 
-    try {
+    if (inMemory[movieSearchcity] !== undefined) {
+        console.log(' cache hit , data in cache memory');
+        response.send(inMemory[movieSearchcity]);
+    }
 
-        axios.get(moviesURL).then((moviesDataResults) => {
-            console.log('inside axios');
-            console.log(moviesDataResults.data.results)
-            const moviesDataArray = moviesDataResults.data.results.map((item) => { return new movieShow(item); }
-            )
-            response.send(moviesDataArray)
-        })
+    else{
+        console.log(' cache miss , send req to themoviedb API');
+        try {
+
+            axios.get(moviesURL).then((moviesDataResults) => {
+                console.log('inside axios');
+                // console.log(moviesDataResults.data.results)
+                const moviesDataArray = moviesDataResults.data.results.map((item) => { return new movieShow(item); }
+                )
+                response.send(moviesDataArray)
+            })
+        }
+        catch (error) {
+            console.log('error from axios', error)
+            response.status(404).send(error)
+        }
     }
-    catch (error) {
-        console.log('error from axios', error)
-        response.status(404).send(error)
     }
-}
+    
 
 class movieShow {
     constructor(item) {
